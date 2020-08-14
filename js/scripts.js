5079b281495b83197087af0d1831837b034f6eba
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-14 10:01:01 -0700

    it will make sure all the variables are gathering there inputs. now working on branching statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
-$(document).ready(function() {
-$("form#data").submit(function(event) {
-  const name = $("input#name").val();
-  const hobby = $("#hobby").val();
-  const animal = $("input:radio[name=animal]:checked").val();
-  const color = $("#color").val();
-  const book =$("input:radio[name=book]:checked").val();
-  event.preventDefault();
+$(document).ready(function () {
+  $("#data").submit(function (event) {
+    const name = $("#name").val();
+    const hobby = parseInt($("#hobby").val());
+    const animal = $("input:radio[name=animal]:checked").val();
+    const color = $("#color").val();
+    const book =$("input:radio[name=book]:checked").val();
+    event.preventDefault();
+    if( hobby = 2){
+      $('#python').show();
+    }
 });
 });
