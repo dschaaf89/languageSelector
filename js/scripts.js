ab99809a69b89a451af35b0cab6f5541c766bc52
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-14 09:15:01 -0700

    it will finish the survey questions on the index.html page. also it will make sure all variable inputs are in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ $("form#data").submit(function(event) {
   const name = $("input#name").val();
   const hobby = $("#hobby").val();
   const animal = $("input:radio[name=animal]:checked").val();
+  const color = $("#color").val();
+  const book =$("input:radio[name=book]:checked").val();
   event.preventDefault();
 });
 });
